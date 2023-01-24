30c1c5384415ae73a22c4be81072f562705fe85e
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2023-01-24 19:54:53 +0700

    Create jumbotron

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 import React from 'react';
 import style from '../styles/pages/index.module.css';
 import HTMLHead from '@/components/Head';
-import Button from "@/components/atom/Button";
+import Button from '@/components/atom/Button';
 
 export default function Home() {
   return (
     <>
-      <HTMLHead />
+      <HTMLHead title="Autover" />
       <section id="s-navbar" className={style.sNavbar}>
         <nav className="d-flex jc-space-between ai-center h-100">
           <div className="left-side">
@@ -19,7 +19,22 @@ export default function Home() {
           </div>
         </nav>
       </section>
-      <section id="s-jumbotron" className={style.sJumbotron} />
+      <section id="s-jumbotron" className={`${style.sJumbotron} d-grid`}>
+        <div className={`left-side ${style.jumbotronLeftSide}`}>
+          <div className="d-flex flex-column w-100 h-100 jc-center ai-start">
+            <h1 className={style.jumbotronHeading}>Rental Mobil Murah untuk Setiap Kebutuhan</h1>
+            <p className={style.jumbotronText}>
+              Dapatkan tarif sewa mobil paling terjangkau disini.
+              Kami menyediakan banyak pilihan mobil seperti, MPV, SUV, Crossover, dll.
+            </p>
+          </div>
+        </div>
+        <div className="right-side">
+          <div className="d-flex w-100 h-100 jc-center ai-center">
+            <Image src="/images/jumbotron-image.png" alt="jumbotron image" height={260} width={520} />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
