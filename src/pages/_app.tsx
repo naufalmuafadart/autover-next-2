33c93f1f6bed9099cecf83a3e34f2ce05f9318fc
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2023-01-25 06:38:28 +0700

    import css in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import '@/styles/global.css';
+import '@/styles/library/button.css';
+import '@/styles/library/layouting.css';
+import '@/styles/library/sizing.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
