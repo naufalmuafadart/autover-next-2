2163cf0629acae503d2afb6e15a687483798875a
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2023-01-25 09:38:32 +0700

    Make tab in tutorial section to be work

---
+++ src/components/organism/Tutorial/data.js
@@ -0,0 +1,46 @@
+export default {
+  renter: [
+    {
+      orderNumber: 1,
+      mainText: 'Pilih tempat dan tanggal penyewaan',
+      info: 'Pilih lokasi dan tanggal kamu akan menyewa mobil',
+    },
+    {
+      orderNumber: 2,
+      mainText: 'Tentukan mobil yang akan kamu sewa',
+      info: 'Pilih salah satu mobil yang ingin kamu sewa',
+    },
+    {
+      orderNumber: 3,
+      mainText: 'Checkout Pesanan',
+      info: 'Lengkapi data pada halaman checkout, dan bayar pesanan kamu',
+    },
+    {
+      orderNumber: 4,
+      mainText: 'Nikmati Mobilnya',
+      info: 'Kemudikan mobilnya',
+    },
+  ],
+  host: [
+    {
+      orderNumber: 1,
+      mainText: 'Daftar jadi Host',
+      info: 'Buka menu \'Jadi Host\'',
+    },
+    {
+      orderNumber: 2,
+      mainText: 'Lengkapi data',
+      info: 'Pastikan diisi dengan benar',
+    },
+    {
+      orderNumber: 3,
+      mainText: 'Tambah data mobil',
+      info: 'Tambah data semua mobil',
+    },
+    {
+      orderNumber: 4,
+      mainText: 'Tunggu sampai ada penyewa',
+      info: 'Pastikan mobilnya selalu dalam keadaan prima',
+    },
+  ],
+};
--- src/components/organism/Tutorial/index.tsx
+++ src/components/organism/Tutorial/index.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import style from '../../../styles/component/organism/tutorial.module.css';
 import Button from '@/components/atom/Button';
 import ListItem from '@/components/organism/Tutorial/ListItem';
+import data from './data';
 
 export default function Tutorial() {
+  const [activeTab, setActiveTab] = useState('renter');
+
+  const onBtnRenterClick = () => {
+    setActiveTab('renter');
+  };
+
+  const onBtnHostClick = () => {
+    setActiveTab('host');
+  };
+
   return (
     <section id="s-tutorial" className={`d-grid ${style.sTutorial}`}>
       <div className="left-side d-flex jc-center ai-center">
@@ -13,30 +24,36 @@ export default function Tutorial() {
       <div className="right-side">
         <h1 className={style.mb30}>Cara sewa mobil di Autover</h1>
         <div id="button-container" className={`${style.buttonContainer} ${style.mb30}`}>
-          <Button text="Penyewa" className={style.btnRenter} />
-          <Button text="Host" isSecondary />
+          <Button
+            text="Penyewa"
+            className={style.btnRenter}
+            isSecondary={activeTab !== 'renter'}
+            onClick={onBtnRenterClick}
+          />
+          <Button
+            text="Host"
+            isSecondary={activeTab !== 'host'}
+            onClick={onBtnHostClick}
+          />
         </div>
         <ul className={style.ul}>
-          <ListItem
-            orderNumber={1}
-            mainText="Pilih tempat dan tanggal penyewaan"
-            info="Pilih lokasi dan tanggal kamu akan menyewa mobil"
-          />
-          <ListItem
-            orderNumber={2}
-            mainText="Tentukan mobil yang akan kamu sewa"
-            info="Pilih salah satu mobil yang ingin kamu sewa"
-          />
-          <ListItem
-            orderNumber={3}
-            mainText="Checkout Pesanan"
-            info="Lengkapi data pada halaman checkout, dan bayar pesanan kamu"
-          />
-          <ListItem
-            orderNumber={4}
-            mainText="Nikmati Mobilnya"
-            info="Kemudikan mobilnya"
-          />
+          {
+            activeTab === 'renter' ? data.renter.map((listItem) => (
+              <ListItem
+                key={listItem.orderNumber}
+                orderNumber={listItem.orderNumber}
+                mainText={listItem.mainText}
+                info={listItem.info}
+              />
+            )) : data.host.map((listItem) => (
+              <ListItem
+                key={listItem.orderNumber}
+                orderNumber={listItem.orderNumber}
+                mainText={listItem.mainText}
+                info={listItem.info}
+              />
+            ))
+          }
         </ul>
         <div id="button-container" className={style.buttonContainer}>
           <Button text="Cari Mobil" />
