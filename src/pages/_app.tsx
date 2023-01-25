418112e29801ddfbc528eef902050952f4406e1b
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2023-01-25 19:21:50 +0700

    Create login page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,19 @@ import React from 'react';
 import '@/styles/global.css';
 import '@/styles/library/button.css';
 import '@/styles/library/font.css';
+import '@/styles/library/form.css';
 import '@/styles/library/layouting.css';
 import '@/styles/library/sizing.css';
 import type { AppProps } from 'next/app';
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({ weight: ['100', '300', '400', '500', '700', '900'], subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <main className={roboto.className} id="mainDocument">
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </main>
+  );
 }
---
+++ src/pages/login.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import Head from '@/components/Head';
+import style from '@/styles/pages/login.module.css';
+import Image from 'next/image';
+import localFont from '@next/font/local';
+import Button from '@/components/atom/Button';
+
+const michroma = localFont({ src: './font/Michroma/Michroma-Regular.ttf' });
+
+export default function Login() {
+  useEffect(() => {
+    const body = document.getElementsByTagName('body')[0];
+    body.style.backgroundColor = 'blue';
+    body.style.display = 'flex';
+    body.style.justifyContent = 'center';
+    body.style.height = '100vh';
+  });
+
+  return (
+    <>
+      <Head title="Login" />
+      <div className={style.container}>
+        <div className={style.leftSide}>
+          {/* eslint-disable-next-line jsx-a11y/img-redundant-alt,@next/next/no-img-element */}
+          <img src="/images/img-login.png" alt="image login" className={style.image} />
+        </div>
+        <div className={style.rightSide}>
+          <form className="d-flex flex-column ai-center">
+            <div className={`${style.brand} d-flex jc-start ai-center`}>
+              <Image src="/images/logo.png" alt="Logo" width={70} height={70} className={style.logo} />
+              <p className={`${michroma.className} ${style.brandText}`}>AUTOVER</p>
+            </div>
+
+            <h1 className={style.h1}>Login</h1>
+            <div className="form-group group-column">
+              <label htmlFor="inputEmail">Email</label>
+              <input type="email" id="inputEmail" required />
+              <small>Email salah</small>
+            </div>
+
+            <div className="form-group group-column">
+              <label htmlFor="inputPassword">Password</label>
+              <input type="email" id="inputPassword" required />
+              <small>Password salah</small>
+            </div>
+
+            <Button text="Login" className={`${style.btnSubmit} w-100`} />
+
+            <div className={style.alternativeContainer}>
+              <p className="alternative">
+                <span>Sudah punya akun?</span>
+                {' '}
+                <a href="/signup" className={style.alternativeContainerAnchor}>Daftar disini</a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
