e185d822ce28c47ea814e52379be3e940dc48198
naufalmuafadart <mnaufalmuafa.dart@gmail.com>
2023-01-25 19:53:01 +0700

    import michroma

--- src/components/organism/Footer/index.tsx
+++ src/components/organism/Footer/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
 import style from '@/styles/component/organism/footer.module.css';
+import localFont from '@next/font/local';
+
+const michroma = localFont({ src: '../../../pages/font/Michroma/Michroma-Regular.ttf' });
 
 export default function Footer() {
   return (
@@ -8,7 +11,7 @@ export default function Footer() {
       <footer className="d-flex jc-space-between ai-center">
         <div className="left-side d-flex jc-start ai-center">
           <Image src="/images/logo.png" alt="Logo" width={70} height={70} className={style.image} />
-          <p>AUTOVER</p>
+          <p className={`${michroma.className} ${style.brandName}`}>AUTOVER</p>
         </div>
         <div className="right-side">
           <p>© 2023 Autover</p>
