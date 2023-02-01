800bd833dcb17ac5ee30a0df918aa286944f8d8a
minidart <opangay@gmail.com>
2023-02-01 09:32:57 +0700

    Fix signup and signin service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,16 +1,23 @@
 import axios from 'axios';
+import qs from 'qs';
 import { signUpPayload, signInPayload } from '@/services/dataTypes';
 
 export async function signUp({
   full_name, phone_number, email, password,
 }: signUpPayload) {
   try {
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_EXPRESS_END_POINT}/auth/signup`,
-      {
-        full_name, phone_number, email, password,
+    const data = qs.stringify({
+      full_name, phone_number, email, password,
+    });
+    const config = {
+      method: 'post',
+      url: `${process.env.NEXT_PUBLIC_EXPRESS_END_POINT}/api/auth/signup`,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-    );
+      data,
+    };
+    const response = await axios(config);
     return response.data;
   } catch (error) {
     // @ts-ignore
@@ -22,12 +29,18 @@ export async function signIn({
   email, password,
 }: signInPayload) {
   try {
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_EXPRESS_END_POINT}/auth/signin`,
-      {
-        email, password,
+    const data = qs.stringify({
+      email, password,
+    });
+    const config = {
+      method: 'post',
+      url: `${process.env.NEXT_PUBLIC_EXPRESS_END_POINT}/api/auth/signin`,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-    );
+      data,
+    };
+    const response = await axios(config);
     return response.data;
   } catch (error) {
     // @ts-ignore
