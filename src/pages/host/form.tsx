3afe93ee5f6b567e50d14390101a8f73abe31a7d
minidart <opangay@gmail.com>
2023-02-08 09:01:05 +0700

    Fix form host layout

--- src/pages/host/form.tsx
+++ src/pages/host/form.tsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import HTMLHead from '@/components/Head';
-import Navbar from '@/components/organism/Navbar';
 import Button from '@/components/atom/Button';
 import style from '@/styles/pages/host/form.module.css';
 import { useRouter } from 'next/router';
@@ -9,13 +7,14 @@ import { postHost, getCheckIsAHost } from '@/services/host';
 import { district } from '@/services/dataTypes';
 import { GetServerSideProps as propsGetServerSide } from '@/services/dataTypes/props';
 import { postHostPayload } from '@/services/dataTypes/payload';
+import RenterPageTemplate from '@/components/page/RenterPageTemplate';
 
 interface HostProps {
   districts: Array<district>;
   accessToken: string;
 }
 
-export default function Host(props: HostProps) {
+export default function FormHost(props: HostProps) {
   const { districts, accessToken } = props;
   const [districtId, setDistrictId] = useState(districts[0]._id);
   const router = useRouter();
@@ -34,38 +33,36 @@ export default function Host(props: HostProps) {
   };
 
   return (
-    <>
-      <HTMLHead title="Form Host" />
-      <div id="mainContent">
-        <Navbar isLogin />
-        <hr />
-        <section>
-          <form onSubmit={onSubmit}>
-            <h1 className={style.h1}>Pendaftaran Host</h1>
-            <div className="group-row">
-              <div className="group-row-row">
-                <label>Kecamatan</label>
-                <select name="district_id" onChange={onDistrictChange}>
-                  {
-                    districts.map((d: district) => (
-                      <option
-                        key={d._id}
-                        value={d._id}
-                        defaultValue={districtId}
-                      >
-                        {d.name}
-                      </option>
-                    ))
-                  }
-                </select>
-              </div>
+    <RenterPageTemplate
+      isLogin
+      pageTitle="Form host"
+    >
+      <section>
+        <form onSubmit={onSubmit}>
+          <h1 className={style.h1}>Pendaftaran Host</h1>
+          <div className="group-row">
+            <div className="group-row-row">
+              <label>Kecamatan</label>
+              <select name="district_id" onChange={onDistrictChange}>
+                {
+                  districts.map((d: district) => (
+                    <option
+                      key={d._id}
+                      value={d._id}
+                      defaultValue={districtId}
+                    >
+                      {d.name}
+                    </option>
+                  ))
+                }
+              </select>
             </div>
+          </div>
 
-            <Button text="Submit" type="submit" className="float-right" />
-          </form>
-        </section>
-      </div>
-    </>
+          <Button text="Submit" type="submit" className="float-right" />
+        </form>
+      </section>
+    </RenterPageTemplate>
   );
 }
 
